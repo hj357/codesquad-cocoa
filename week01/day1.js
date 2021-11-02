45635b818a05154968bec670f2cc7742231e59a6
HJ <jkdsa9@gmail.com>
2021-11-02 11:21:02 +0900

    finish

--- week01/day1.js
+++ week01/day1.js
@@ -7,6 +7,12 @@ function getArea(shape, num1, num2, num3) {
   switch (shape) {
     case "circle":
       if ((num2 && num1, num2)) {
+        let sum = 0;
+        for (let i = num1; i < num2; i++) {
+          let d = i * i * 3.14;
+          sum = sum + d;
+        }
+        console.log(sum);
       } else {
         let a = num1 * 3.14;
         console.log(a.toFixed(2));
@@ -28,13 +34,5 @@ function getArea(shape, num1, num2, num3) {
 getArea("circle", 10); //31.40
 getArea("rect", 10, 15); //150
 getArea("trapezoid", 10, 15, 12); //900
-getArea("circl", 10); //정확하게 입력해주세요,
-getArea("circle", 1, 11);
-
-function circle2(n, num2) {
-  let sum = 0;
-  for (let i = n; i < num2; i++) {
-    sum = sum + i;
-    console.log(sum);
-  }
-} //작동 잘 안됨 ㅠ
+getArea("circle", 1, 5);
+getArea("circl", 10); //정확하게 입력해주세요
